bac15915cb44d356f927e3e9c2bdb5ce211964e2
Alex Campos <alex.campos@gmail.com>
2023-05-17 01:59:12 -0500

    adding note about powershell parser

--- init.lua
+++ init.lua
@@ -6,3 +6,14 @@ if vim.g.vscode then
 else
   require("config.lazy")
 end
+
+-- if needed, this may be a location where to install custom parser for powrshell.
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_config.powershell = {
+--   install_info = {
+--     url = "~/dev/personal/tree-sitter-PowerShell",
+--     files = { "src/scanner.c" },
+--   },
+--   filetype = "ps1",
+--   used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" },
+-- }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,66 @@
 return {
   {
+    "mfussenegger/nvim-treehopper",
+    keys = { { "m", mode = { "o", "x" } } },
+    config = function()
+      vim.cmd([[
+        omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>
+        xnoremap <silent> m :lua require('tsht').nodes()<CR>
+      ]])
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
         "bash",
         "c",
         "cmake",
+        "comment", -- comments are slowing down TS bigtime, so disable for now
+        "cpp",
+        "css",
+        "diff",
         "gitignore",
         "go",
+        "graphql",
         "html",
+        "http",
         "javascript",
         "jsdoc",
         "jsonc",
-        "json",
         "lua",
         "markdown",
         "markdown_inline",
+        "nix",
+        "php",
         "python",
         "query",
         "regex",
         "rust",
+        "sql",
+        "toml",
         "tsx",
         "typescript",
-        "toml",
         "vim",
         "yaml",
+        "json",
       },
+      matchup = {
+        enable = true,
+      },
+      highlight = { enable = true },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "windwp/nvim-ts-autotag", opts = {} },
     },
   },
 }
