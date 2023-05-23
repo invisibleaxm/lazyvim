285e130d117c5cbe0cffcd75e2fba05e13bc506d
Alex Campos <alex.campos@ensono.com>
2023-05-22 21:52:21 -0500

    synching changes

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,5 @@
 return {
   "folke/trouble.nvim",
   -- opts will be merged with the parent spec
-  opts = { auto_preview = false, auto_open = true },
+  opts = { auto_preview = false, auto_open = false },
 }
