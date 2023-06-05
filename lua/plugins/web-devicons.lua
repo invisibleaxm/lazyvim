05ad7949448461db9d42b7743265abf5c554ab41
Alex Campos <me@mail.com>
2023-06-04 22:55:49 -0500

    fixing a few icons

---
+++ lua/plugins/web-devicons.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+    opts = {
+      override = {
+        ps1 = {
+          -- icon = "",
+          icon = "",
+          color = "#4273ca",
+          cterm_color = "68",
+          name = "PsScriptfile",
+        },
+        ["dockerfile"] = {
+          icon = "",
+          color = "#458ee6",
+          cterm_color = "68",
+          name = "Dockerfile",
+        },
+        [".dockerignore"] = {
+          icon = "",
+          color = "#458ee6",
+          cterm_color = "68",
+          name = "Dockerfile",
+        },
+
+        -- ["ps1"] = {
+        --   icon = "",
+        --   color = "#4273ca",
+        --   cterm_color = "68",
+        --   name = "PsScriptfile",
+        -- },
+      },
+    },
+  },
+}
