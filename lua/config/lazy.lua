57360ae9c8f4f912ef7d5c82ab22535c4a8f0bf3
Alex Campos <alex.campos@gmail.com>
2023-06-12 18:41:10 -0500

    adding co-pilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.util.project" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
-  { "ggandor/flit.nvim", enabled = false }, -- this plugin takes over the find and . , which i was not too keen
+  -- { "ggandor/flit.nvim", enabled = false }, -- this plugin takes over the find and . , which i was not too keen
   -- { "rcarriga/nvim-notify", enabled = false },
   --  { "echasnovski/mini.pairs", enabled = false },
 }
