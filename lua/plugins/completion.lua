65f2de02787badcec387aad5f58ea50b5d1ec7ea
Alex Campos <alex.campos@gmail.com>
2023-06-16 16:47:51 -0500

    improving mini pairs

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,4 +1,5 @@
 return {
+
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- { "ggandor/flit.nvim", enabled = false }, -- this plugin takes over the find and . , which i was not too keen
   -- { "rcarriga/nvim-notify", enabled = false },
   --  { "echasnovski/mini.pairs", enabled = false },
+  test,
 }
