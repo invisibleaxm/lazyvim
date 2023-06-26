a92595b15305b9a9d13b5851523da4f9a04682df
Alex Campos <alex.campos@gmail.com>
2023-06-26 15:41:04 -0600

    hiding dotfiles

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,7 +5,7 @@ return {
       open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
       filesystem = {
         filtered_items = {
-          hide_dotfiles = false,
+          hide_dotfiles = true,
         },
       },
     },
